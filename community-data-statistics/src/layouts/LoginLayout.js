dc08d36322b4c4454e5a663405ea654cd5e9e792
Glassay <31647920+Glassay@users.noreply.github.com>
2018-05-09 19:30:11 +0800

    登录界面

--- community-data-statistics/src/layouts/LoginLayout.js
+++ community-data-statistics/src/layouts/LoginLayout.js
@@ -3,4 +3,66 @@
  * 登录界面
  */
 
- 
+import React from 'react';
+// import { connect } from 'dva';
+import { Layout, Form, Icon, Input, Button, Checkbox } from 'antd';
+
+import styles from './LoginLayout.less';
+
+const FormItem = Form.Item;
+const { Content } = Layout;
+
+class LoginLayout extends React.Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+        this.props.dispatch({
+          type: 'login/adminLogin',
+          payload: values,
+        })
+      }
+    });
+  }
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <Layout className={styles.layout}>
+        <Content className={styles.bgimage}>
+          <Form onSubmit={this.handleSubmit} className={styles.loginForm}>
+            <FormItem>
+              {getFieldDecorator('userName', {
+                rules: [{ required: true, message: '请输入用户名！' }],
+              })(
+                <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+              )}
+            </FormItem>
+            <FormItem>
+              {getFieldDecorator('password', {
+                rules: [{ required: true, message: '请输入密码！' }],
+              })(
+                <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+              )}
+            </FormItem>
+            <FormItem>
+              {getFieldDecorator('remember', {
+                valuePropName: 'checked',
+                initialValue: true,
+              })(
+                <Checkbox>Remember me</Checkbox>
+              )}
+              <a className={styles.formForget} href="">Forgot password</a>
+              <Button type="primary" htmlType="submit" className={styles.formButton}>
+                登录
+              </Button>
+            </FormItem>
+          </Form>
+        </Content>
+      </Layout>
+    );
+  }
+}
+LoginLayout = Form.create({})(LoginLayout);
+
+export default LoginLayout;
--- community-data-statistics/src/router.js
+++ community-data-statistics/src/router.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
 import BasicLayout from './layouts/BasicLayout';
+import LoginLayout from './layouts/LoginLayout';
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
+        <Route path="/login" exact component={LoginLayout} />
         <Route path="/city" component={BasicLayout} />
       </Switch>
     </Router>
