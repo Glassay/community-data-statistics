dd308bf197bbb3a9d98cfb6a94c32add8a2c3345
Glassay <31647920+Glassay@users.noreply.github.com>
2018-05-10 10:11:22 +0800

    图表

--- community-data-statistics/src/routes/Population/index.js
+++ community-data-statistics/src/routes/Population/index.js
@@ -7,12 +7,30 @@ import React from 'react';
 import {
   Cascader,
 } from 'antd';
+import { Chart, Geom, Axis, Tooltip, Legend, Coord } from 'bizcharts';
+
+import styles from './index.less';
 
 class Population extends React.Component {
   handleChange = (value) => {
     console.log(value);
   }
   render() {
+    // 数据源
+    const data = [
+      { genre: 'Sports', sold: 275, income: 2300 },
+      { genre: 'Strategy', sold: 115, income: 667 },
+      { genre: 'Action', sold: 120, income: 982 },
+      { genre: 'Shooter', sold: 350, income: 5271 },
+      { genre: 'Other', sold: 150, income: 3710 }
+    ];
+    
+    // 定义度量
+    const cols = {
+      sold: { alias: '销售量' },
+      genre: { alias: '游戏种类' }
+    };
+
     const options = [{
       value: '保定',
       label: '保定',
@@ -47,7 +65,16 @@ class Population extends React.Component {
       }],
     }]
     return (
-      <Cascader options={options} onChange={() => this.handleChange} placeholder="选择地区" />
+      <div>
+        <Cascader options={options} onChange={() => this.handleChange} placeholder="选择地区" />
+        <Chart className={styles.chart} width={600} height={400} data={data} scale={cols}>
+          <Axis name="genre" />
+          <Axis name="sold" />
+          <Legend position="bottom" dy={-20} />
+          <Tooltip />
+          <Geom type="interval" position="genre*sold" color="genre" />
+        </Chart>
+      </div>
     );
   }
 }
