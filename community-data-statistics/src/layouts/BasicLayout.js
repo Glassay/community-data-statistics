0a585e86aece864745e87e7a3f81cb3332f564ef
Glassay <31647920+Glassay@users.noreply.github.com>
2018-05-23 16:12:44 +0800

    用户登录拦截

--- community-data-statistics/src/layouts/BasicLayout.js
+++ community-data-statistics/src/layouts/BasicLayout.js
@@ -4,6 +4,7 @@
  */
 
 import React from 'react';
+import { connect } from 'dva';
 import { Layout, Menu, Icon, Breadcrumb } from 'antd';
 import { Switch, Route, Link } from 'dva/router';
 
@@ -19,104 +20,131 @@ const { Header, Content, Footer, Sider } = Layout;
 const SubMenu = Menu.SubMenu;
 
 class BasicLayout extends React.Component {
+  componentDidMount() {
+    console.log('localStorage++++', localStorage);
+  }
   state = {
     collapsed: false,
   };
+
   onCollapse = (collapsed) => {
     console.log(collapsed);
     this.setState({ collapsed });
   }
+
+  loginOut = () => {
+    this.props.dispatch({
+      type: 'users/loginOut'
+    })
+  }
+
   render() {
+    const isLogin = localStorage.getItem('isLogin');
+    console.log('asdasdasd', isLogin);
     return (
-      <Layout style={{ minHeight: '100vh' }}>
-        <Sider
-          collapsible
-          collapsed={this.state.collapsed}
-          onCollapse={this.onCollapse}
-        >
-          <div className={styles.logo}>
-            <Link to="/">
-              <h1>城市社区管理</h1>
-            </Link>
-          </div>
-          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-            <SubMenu
-              key="sub1"
-              title={<span><Icon type="area-chart" /><span>城市综合数据</span></span>}
+      <Layout style={{ minHeight: '100vh', backgroundColor: '#fff' }}>
+        {
+          isLogin ?
+          <Layout>
+            <Sider
+              collapsible
+              collapsed={this.state.collapsed}
+              onCollapse={this.onCollapse}
             >
-              <Menu.Item key="1">
-                <Link to="/city/population">人口数据</Link>
-              </Menu.Item>
-              <Menu.Item key="2">
-                <Link to="/city/economics">经济数据</Link>
-              </Menu.Item>
-              <Menu.Item key="3">
-                <Link to="/city/industrial">工业数据</Link>
-              </Menu.Item>
-              <Menu.Item key="4">
-                <Link to="/city/weather">天气展示</Link>
-              </Menu.Item>
-            </SubMenu>
-            <SubMenu
-              key="sub2"
-              title={<span><Icon type="table" /><span>社区管理</span></span>}
-            >
-              <Menu.Item key="5">
-                <Link to="/city/community-population">人员数据</Link>
-              </Menu.Item>
-              <Menu.Item key="6">
-                <Link to="/city/community-enterprise">企业数据</Link>
-              </Menu.Item>
-            </SubMenu>
-          </Menu>
-        </Sider>
-        <Layout>
-          <Header>
-            <Menu theme="dark" mode="horizontal">
-              <SubMenu
-                style={{
-                  float: 'right',
-                  right: 20,
-                  top: 8
-                }}
-                title={
-                  <span>
-                    <Icon type="user" />
-                    管理员
-                  </span>
-                }
-              >
-                <Menu.Item key="logout">
-                  退出登录
-                </Menu.Item>
-              </SubMenu>
-            </Menu>
-          </Header>
-          <Content style={{ margin: '0 16px' }}>
-            <Breadcrumb style={{ margin: '16px 0' }}>
-              <Breadcrumb.Item><a href="/city/population">人口数据</a></Breadcrumb.Item>
-              <Breadcrumb.Item><a href="/city/economics">经济数据</a></Breadcrumb.Item>
-              <Breadcrumb.Item><a href="/city/industrial">工业数据</a></Breadcrumb.Item>
-              <Breadcrumb.Item><a href="/city/weather">天气展示</a></Breadcrumb.Item>
-            </Breadcrumb>
-            <div style={{ padding: 24, background: '#fff', minHeight: '80vh' }}>
-              <Switch>
-                <Route path="/city/population" render={() => <Population />} />
-                <Route path="/city/economics" render={() => <Economics />} />
-                <Route path="/city/industrial" render={() => <Industrial />} />
-                <Route path="/city/weather" render={() => <Weather />}/>
-                <Route path="/city/community-population" render={() => <CPopulation />} />
-                <Route path="/city/community-enterprise" render={() => <Enterprise />} />
-              </Switch>
+              <div className={styles.logo}>
+                <Link to="/city">
+                  <h1>城市社区管理</h1>
+                </Link>
+              </div>
+              <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+                <SubMenu
+                  key="sub1"
+                  title={<span><Icon type="area-chart" /><span>城市综合数据</span></span>}
+                >
+                  <Menu.Item key="1">
+                    <Link to="/city/population">人口数据</Link>
+                  </Menu.Item>
+                  <Menu.Item key="2">
+                    <Link to="/city/economics">经济数据</Link>
+                  </Menu.Item>
+                  <Menu.Item key="3">
+                    <Link to="/city/industrial">工业数据</Link>
+                  </Menu.Item>
+                  <Menu.Item key="4">
+                    <Link to="/city/weather">天气展示</Link>
+                  </Menu.Item>
+                </SubMenu>
+                <SubMenu
+                  key="sub2"
+                  title={<span><Icon type="table" /><span>社区管理</span></span>}
+                >
+                  <Menu.Item key="5">
+                    <Link to="/city/community-population">人员数据</Link>
+                  </Menu.Item>
+                  <Menu.Item key="6">
+                    <Link to="/city/community-enterprise">企业数据</Link>
+                  </Menu.Item>
+                </SubMenu>
+              </Menu>
+            </Sider>
+            <Layout>
+              <Header>
+                <Menu theme="dark" mode="horizontal">
+                  <SubMenu
+                    style={{
+                      float: 'right',
+                      right: 20,
+                      top: 8
+                    }}
+                    title={
+                      <span>
+                        <Icon type="user" />
+                        管理员
+                      </span>
+                    }
+                  >
+                    <Menu.Item key="logout">
+                      <span onClick={this.loginOut}>退出登录</span>
+                    </Menu.Item>
+                  </SubMenu>
+                </Menu>
+              </Header>
+              <Content style={{ margin: '0 16px' }}>
+                <Breadcrumb style={{ margin: '16px 0' }}>
+                  <Breadcrumb.Item><a href="/city/population">人口数据</a></Breadcrumb.Item>
+                  <Breadcrumb.Item><a href="/city/economics">经济数据</a></Breadcrumb.Item>
+                  <Breadcrumb.Item><a href="/city/industrial">工业数据</a></Breadcrumb.Item>
+                  <Breadcrumb.Item><a href="/city/weather">天气展示</a></Breadcrumb.Item>
+                </Breadcrumb>
+                <div style={{ padding: 24, background: '#fff', minHeight: '80vh' }}>
+                  <Switch>
+                    <Route path="/city/population" render={() => <Population />} />
+                    <Route path="/city/economics" render={() => <Economics />} />
+                    <Route path="/city/industrial" render={() => <Industrial />} />
+                    <Route path="/city/weather" render={() => <Weather />}/>
+                    <Route path="/city/community-population" render={() => <CPopulation />} />
+                    <Route path="/city/community-enterprise" render={() => <Enterprise />} />
+                  </Switch>
+                </div>
+              </Content>
+              <Footer style={{ textAlign: 'center' }}>
+                Copyright ©2018 Created by Glassay
+              </Footer>
+            </Layout>
+          </Layout>
+          :
+          <Link to="/login">
+            <div className={styles.needLogin}>
+              <h1 style={{ marginTop: 100, color: '#f6ec66' }}>点击登录</h1>
+              <img src="http://www.gx8899.com/uploads/allimg/2016060812/52o34owzgd0.jpg" alt="" />
             </div>
-          </Content>
-          <Footer style={{ textAlign: 'center' }}>
-            Copyright ©2018 Created by Glassay
-          </Footer>
-        </Layout>
+          </Link>
+        }
       </Layout>
     );
   }
 }
 
-export default BasicLayout;
+export default connect(state => ({
+  status: state.users.status
+}))(BasicLayout);
--- community-data-statistics/src/models/users.js
+++ community-data-statistics/src/models/users.js
@@ -7,8 +7,9 @@ export default {
   state: {
     userInfo: {
       userName: '111',
-      password: '111'
-    }
+      password: '111',
+    },
+    // status: false
   },
 
   effects: {
@@ -16,11 +17,21 @@ export default {
       const loginInfos = yield select(state => state.users.userInfo);
       if(payload.userName === loginInfos.userName && payload.password === loginInfos.password) {
         yield put(routerRedux.push('/city'));
+        yield localStorage.setItem('isLogin', true);
+        console.log('localStorage>>>>>', localStorage.isLogin);
       } else {
         message.error('用户名或密码错误！');
       }
+    },
+
+    *loginOut({ payload }, { put, select }) {
+      yield localStorage.removeItem('isLogin')
+      console.log('loginOUtstatus>>>>', localStorage.isLogin);
+      console.log('loaclStorage++++++', localStorage);
+      yield put(routerRedux.push('/login'))
     }
   },
 
-  reducers: {},
+  reducers: {
+  },
 }
